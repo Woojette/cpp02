4b4a22a3bb3c4fba705078c7b0967129bfca275f
Woojeong <alsxmakt123@naver.com>
2026-04-17 19:31:28 +0200

    ex01 en cours

---
+++ ex01/includes/Fixed.hpp
@@ -0,0 +1,24 @@
+#ifndef	FIXED_HPP
+# define	FIXED_HPP
+
+#include <iostream>
+
+class	Fixed
+{
+	private:
+		int					RawBits;
+		static const int	FractionalBits = 8;
+	public:
+		Fixed();
+        Fixed(const int n);
+        Fixed(const float f);
+		Fixed(const Fixed& other);
+		~Fixed();
+		Fixed&	operator=(const Fixed& other);
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+        float   toFloat(void) const;
+        int     toInt(void) const;
+};
+
+#endif
